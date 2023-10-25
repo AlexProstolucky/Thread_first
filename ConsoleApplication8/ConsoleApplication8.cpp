d27e92bbb71c81182c035edba79bea8feeb694d5
AlexProstolucky <alexprostolucky@gmail.com>
2023-10-25 16:42:36 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication8/ConsoleApplication8.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <thread>
+using namespace std;
+
+void displayHexDigits() {
+    for (int i = 0; i <= 9; ++i) {
+        cout << i << " ";
+    }
+
+    for (char c = 'a'; c <= 'f'; ++c) {
+        cout << c << " ";
+    }
+
+    std::cout << std::endl;
+}
+
+int main() {
+    thread hexThread(displayHexDigits);
+
+    cout << "Main thread is running..." << std::endl;
+
+    hexThread.join();
+
+    cout << "Main thread is done." << std::endl;
+
+    return 0;
+}
